ec2b38a5f4f4ed77e5b5e9e4ba499aae948429eb
jose-marcleiton <j.m.a.m7@hotmail.com>
2025-02-08 21:03:59 -0300

    revisão de conteudo

---
+++ AULA-10 13/01/25/aula1.js
@@ -0,0 +1,41 @@
+
+
+/*ert('olá, mundo!')
+confirm('essa foi uma mensagem do olá mundo')
+console.log('essa foi uma mensagem do olá mundo')
+*/
+/*t nome = 'marcleiton'
+let idade = 29
+let emaill = 'j.m.a.m7@hotmail.com'
+const dataNascimento = '27/06/1995'
+console.log(nome)
+console.log(idade)
+console.log(emaill)
+console.log(dataNascimento)
+*/
+
+/*let nome = prompt('digite seu nome')
+let idade = prompt('digite sua idade')
+let email = prompt('digite seu email')
+let dataNascimento = prompt('digite sua data de nascimento')
+console.log(nome)
+console.log(idade)
+console.log(email)
+console.log(dataNascimento)
+*/
+
+
+// Declarando uma variável com 'let'
+let mensagem = 'Olá, mundo!';
+console.log(mensagem); // Saída: Olá, mundo!
+
+// Alterando o valor da variável
+mensagem = 'Olá, universo!';
+console.log(mensagem); // Saída: Olá, universo!
+
+// Declarando uma variável com 'const' (constante)
+const saudacao = 'Bem-vindo!';
+console.log(saudacao); // Saída: Bem-vindo!
+
+// Tentando alterar o valor de uma constante (isso causará um erro)
+// saudacao = 'Olá!'; // Uncaught TypeError: Assignment to constant variable.
