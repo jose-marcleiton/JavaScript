5d79cd430fd5a26c71c5dda0a1d204e5b457a465
jose-marcleiton <j.m.a.m7@hotmail.com>
2025-02-10 21:54:54 -0300

    novos metodos

---
+++ AULA20/atividade3.js
@@ -0,0 +1,7 @@
+
+let palavras = ['gato', 'passaro', 'capivara', 'serpente', 'papagaio']
+palavras.map(function (palavras) {
+    console.log(`A palavra ${palavras} tem ${palavras.length} letras`)
+
+    return palavras
+})
