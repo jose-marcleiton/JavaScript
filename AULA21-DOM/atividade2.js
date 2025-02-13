a06e32f6e804e20a5ae3400758bd8c0bc8056527
jose-marcleiton <j.m.a.m7@hotmail.com>
2025-02-12 21:49:07 -0300

    dom

---
+++ AULA21-DOM/atividade2.js
@@ -0,0 +1,5 @@
+let itenlista = document.getElementsByClassName('lista')
+console.log(itenlista);
+
+itenlista[0].classe.remove('vermelho');
+itenlista[0].classe.add('azul');
---
+++ AULA21-DOM/dom.js
@@ -0,0 +1,15 @@
+
+/*let itensList = document.getElementsByTagName('li')
+console.log(itensList)
+itensList[0].style.color = 'red'
+
+
+
+let paragrafos = document.getElementsByTagName('p')
+console.log(paragrafos)
+
+
+paragrafos[1].innerHTML = prompt('Digite um texto para o segundo parágrafo')
+*/
+
+
